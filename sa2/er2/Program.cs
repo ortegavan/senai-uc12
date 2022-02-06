3731e738f5809002973f000bc399699edb4f4571
Van Ortega <ortegavan@live.com>
2022-02-06 20:18:04 -0300

    Encontro remoto 2

---
+++ sa2/er2/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+using EncontroRemoto2.Classes;
+
+var pf = new PessoaFisica()
+{
+    Nome = "Vanessa Ortega"
+};
+
+Console.WriteLine($"Nome: {pf.Nome}");
